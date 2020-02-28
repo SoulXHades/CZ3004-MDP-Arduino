4543ab71be1a5033ed3d15fe17ab9dc06f91d5e4
SoulX <haojie_34@hotmail.com>
2020-02-28 11:00:21 +0800

    MDP

---
+++ MDP/mainProgram/motorMovement.ino
@@ -0,0 +1,218 @@
+volatile double rpm_L; //rpm of left motor
+volatile double rpm_R; //rpm of right motor
+volatile double step_L; //number of steps taken by the left motor
+volatile double step_R; //number of steps taken by the right motor
+volatile double error; //difference between left and right steps
+
+volatile const int SPEED =  250; //Set the speed of the motor
+
+// PID Parameters
+volatile double Kp=8, Ki=0, Kd=0.008; //Set PID values //Previous adjusted PID values: Kp=23, Ki=80, Kd=0
+volatile double Output = 0; // Adjusted speed
+
+// Create PID Instance
+PID myPID(&rpm_R, &Output, &rpm_L, Kp, Ki, Kd, DIRECT);
+/*Where parameters of the function are:
+myPID(input(rpm), output(speed), setpoint(rpm), proportional value, intergral value, derative value, motor direction)*/
+
+// for obstacle avoidance checklist
+void avoid(){
+  PS2_obstacleDetection();
+
+  if(PS2_value == 2)
+  {
+    if (secondAvoid)
+      return;
+    else
+    {
+      motorStop();
+      delay(100);
+      rightTurn(45);
+      delay(100);
+      forward(3);
+      delay(100);
+      leftTurn(45);
+      delay(100);
+      forward(5);
+      delay(100);
+      leftTurn(45);
+      delay(100);
+      forward(4);
+      delay(100);
+      rightTurn(45);
+      motorStop();
+
+      // hardcode stop before the end of the arena
+      secondAvoid = true;
+      }
+  }
+}
+
+void forward(double gridNumber){
+  
+  double totalSteps = totalRegularSteps(getFinalSteps(gridNumber));
+
+  motorStart();
+  for(int i=0; i<=SPEED; i++){
+    md.setSpeeds(i, i);
+  }
+  
+  while (step_R <= totalSteps || step_L <= totalSteps ) {
+  computeRPM();
+  myPID.Compute();
+  error = step_L - step_R;
+  double adjust = (error!=0) ? (error>0 ? 1 : -1) : 0;
+  md.setSpeeds(SPEED + adjust + Output, SPEED - adjust);
+  }
+
+  motorStop();
+  delay(10);
+  }
+
+void backward(double gridNumber){
+  double totalSteps = totalRegularSteps(getFinalSteps(gridNumber));
+  
+  motorStart();
+  for(int i=0; i<=SPEED; i++){
+    md.setSpeeds(-i, -i);
+  }
+  
+  while (step_R <= totalSteps || step_L <= totalSteps ) {
+    Serial.print(step_R);
+  computeRPM();
+  myPID.Compute();
+  error = step_L - step_R;
+  double adjust = (error!=0) ? (error>0 ? 1 : -1) : 0;
+  md.setSpeeds(-SPEED - adjust - Output, -SPEED + adjust);
+  }
+
+  motorStop();
+  delay(10);
+  }
+
+void leftTurn(double degree){
+  
+  double totalSteps = totalAngularSteps_L(degree);
+
+  motorStart();
+  for(int i=0; i<=SPEED; i++){
+    md.setSpeeds(i, -i);
+  }
+  
+  while (step_R <= totalSteps || step_L <= totalSteps ) {
+  computeRPM();
+  myPID.Compute();
+  error = step_L - step_R;
+  double adjust = (error!=0) ? (error>0 ? 1 : -1) : 0;
+  md.setSpeeds(SPEED + adjust + Output, -SPEED + adjust);
+  }
+
+  motorStop();
+  delay(10);
+  }
+
+void rightTurn(double degree){
+  double totalSteps = totalAngularSteps_R(degree);
+
+  motorStart();
+  for(int i=0; i<=SPEED; i++){
+    md.setSpeeds(-i, i);
+  }
+  
+  while (step_R <= totalSteps || step_L <= totalSteps ) {
+  computeRPM();
+  myPID.Compute();
+  error = step_L - step_R;
+  double adjust = (error!=0) ? (error>0 ? 1 : -1) : 0;
+  md.setSpeeds(-SPEED - adjust - Output, SPEED - adjust);
+  }
+
+  motorStop();
+  delay(10);
+  }
+  
+// for obstacle avoidance checklist
+void forwardTest(){
+  
+  double totalSteps = totalRegularSteps(fifteen_grid);
+
+  motorStart();
+  for(int i=0; i<=SPEED; i++){
+    md.setSpeeds(i, i);
+  }
+  
+  while (step_R <= totalSteps || step_L <= totalSteps ) {
+    avoid();
+    if(PS2_value == 2 && secondAvoid)
+      break;
+  computeRPM();
+  myPID.Compute();
+  error = step_L - step_R;
+  double adjust = (error!=0) ? (error>0 ? 1 : -1) : 0;
+  md.setSpeeds(SPEED + adjust + Output, SPEED - adjust);
+  }
+
+  motorStop();
+  delay(10);
+  }
+
+void backwardTest(){
+  double totalSteps = totalRegularSteps(fifteen_grid);
+
+  motorStart();
+  for(int i=0; i<=SPEED; i++){
+    md.setSpeeds(-i, -i);
+  }
+  
+  while (step_R <= totalSteps || step_L <= totalSteps ) {
+  computeRPM();
+  myPID.Compute();
+  error = step_L - step_R;
+  double adjust = (error!=0) ? (error>0 ? 1 : -1) : 0;
+  md.setSpeeds(-SPEED - adjust - Output, -SPEED + adjust);
+  }
+
+  motorStop();
+  delay(10);
+  }
+
+void leftTurnTest(){
+  
+  double totalSteps = totalAngularSteps(degree_90);
+
+  motorStart();
+  for(int i=0; i<=SPEED; i++){
+    md.setSpeeds(i, -i);
+  }
+  
+  while (step_R <= totalSteps || step_L <= totalSteps ) {
+  computeRPM();
+  myPID.Compute();
+  error = step_L - step_R;
+  double adjust = (error!=0) ? (error>0 ? 1 : -1) : 0;
+  md.setSpeeds(SPEED + adjust + Output, -SPEED + adjust);
+  }
+
+  motorStop();
+  delay(10);
+  }
+
+void rightTurnTest(){
+  double totalSteps = totalAngularSteps(degree_90);
+
+  motorStart();
+  for(int i=0; i<=SPEED; i++){
+    md.setSpeeds(-i, i);
+  }
+  
+  while (step_R <= totalSteps || step_L <= totalSteps ) {
+  computeRPM();
+  myPID.Compute();
+  error = step_L - step_R;
+  double adjust = (error!=0) ? (error>0 ? 1 : -1) : 0;
+  md.setSpeeds(-SPEED - adjust - Output, SPEED - adjust);
+  }
+
+  motorStop();
+  delay(10);
+  }
