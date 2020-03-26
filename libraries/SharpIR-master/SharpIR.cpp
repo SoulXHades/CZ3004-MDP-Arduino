a4de5dddfea80f722d62b6a689409ea54f52e6c6
SoulX <haojie_34@hotmail.com>
2020-03-26 19:56:31 +0800

    MDP
    1. Removed most of the useless comments from the SharpIR.cpp and modified some of the comments.

--- libraries/SharpIR-master/SharpIR.cpp
+++ libraries/SharpIR-master/SharpIR.cpp
@@ -1,38 +1,3 @@
-/*
-	SharpIR
-
-	Arduino library for retrieving distance (in cm) from the analog GP2Y0A21Y and GP2Y0A02YK
-
-	From an original version of Dr. Marcal Casas-Cartagena (marcal.casas@gmail.com)     
-	
-    Version : 1.0 : Guillaume Rico
-    + Remove average and use median
-    + Definition of number of sample in .h
-    + Define IR pin as input
-
-    Version : 1.1 : Thibaut Mauon
-    + Add SHARP GP2Y0A710K0F for 100cm to 500cm by Thibaut Mauron
-
-	https://github.com/guillaume-rico/SharpIR
-    
-    Original comment from Dr. Marcal Casas-Cartagena :
-   The Sahrp IR sensors are cheap but somehow unreliable. I've found that when doing continous readings to a
-   fix object, the distance given oscilates quite a bit from time to time. For example I had an object at
-   31 cm. The readings from the sensor were mainly steady at the correct distance but eventually the distance
-   given dropped down to 25 cm or even 16 cm. That's quite a bit and for some applications it is quite
-   unacceptable. I checked the library http://code.google.com/p/gp2y0a21yk-library/ by Jeroen Doggen
-   (jeroendoggen@gmail.com) and what the author was doing is to take a bunch of readings and give an average of them
-
-   The present library works similary. It reads a bunch of readings (avg), it checks if the current reading
-   differs a lot from the previous one (tolerance) and if it doesn't differ a lot, it takes it into account
-   for the mean distance.
-   The distance is calculated from a formula extracted from the graphs on the sensors datasheets
-   After some tests, I think that a set of 20 to 25 readings is more than enough to get an accurate distance
-   Reading 25 times and return a mean distance takes 53 ms. For my application of the sensor is fast enough.
-   This library has the formulas to work with the GP2Y0A21Y and the GP2Y0A02YK sensors but exanding it for
-   other sensors is easy enough.
-*/
-
 #ifdef Arduino
   #include "Arduino.h"
 #elif defined(SPARK)
@@ -44,9 +9,8 @@
 // Initialisation function
 //  + irPin : is obviously the pin where the IR sensor is attached
 //  + sensorModel is a int to differentiate the two sensor models this library currently supports:
-//    > 1080 is the int for the GP2Y0A21Y and 
+//    > 1080+ is the int for the GP2Y0A21Y and 
 //    > 20150 is the int for GP2Y0A02YK and 
-//    > 100500 is the long for GP2Y0A710K0F
 //    The numbers reflect the distance range they are designed for (in cm)
 SharpIR::SharpIR(int irPin, long sensorModel) {
   
@@ -109,131 +73,6 @@ void SharpIR::sort(float a[], int size) {
         if (flag) break;
     }
 }
-
-
-/*
-// Original method most people uses
-// Read distance and compute it
-int SharpIR::distance() {
-
-    int ir_val[NB_SAMPLE];
-    int distanceCM;
-    float current;
-
-
-    for (int i=0; i<NB_SAMPLE; i++){
-        // Read analog value
-        ir_val[i] = analogRead(_irPin);
-    }
-    
-    // Sort it 
-    sort(ir_val,NB_SAMPLE);
-
-    
-    if (_model==1080) 
-    {
-        
-        // Different expressions required as the Photon has 12 bit ADCs vs 10 bit for Arduinos
-        #ifdef ARDUINO
-          distanceCM = 29.988 * pow(map(ir_val[NB_SAMPLE / 2], 0, 1023, 0, 5000)/1000.0, -1.173);
-        #elif defined(SPARK)
-          distanceCM = 29.988 * pow(map(ir_val[NB_SAMPLE / 2], 0, 4095, 0, 5000)/1000.0, -1.173);
-        #endif
-
-    } 
-    else if (_model==1081) 
-    {
-        
-        // Different expressions required as the Photon has 12 bit ADCs vs 10 bit for Arduinos
-        #ifdef ARDUINO
-          distanceCM = 29.988 * pow(map(ir_val[NB_SAMPLE / 2], 0, 1023, 0, 5000)/1000.0, -1.173);
-        #elif defined(SPARK)
-          distanceCM = 29.988 * pow(map(ir_val[NB_SAMPLE / 2], 0, 4095, 0, 5000)/1000.0, -1.173);
-        #endif
-
-    } 
-    else if (_model==1082) 
-    {
-        
-        // Different expressions required as the Photon has 12 bit ADCs vs 10 bit for Arduinos
-        #ifdef ARDUINO
-          distanceCM = 29.988 * pow(map(ir_val[NB_SAMPLE / 2], 0, 1023, 0, 5000)/1000.0, -1.173);
-        #elif defined(SPARK)
-          distanceCM = 29.988 * pow(map(ir_val[NB_SAMPLE / 2], 0, 4095, 0, 5000)/1000.0, -1.173);
-        #endif
-
-    } 
-    else if (_model==1083) 
-    {
-        
-        // Different expressions required as the Photon has 12 bit ADCs vs 10 bit for Arduinos
-        #ifdef ARDUINO
-          distanceCM = 29.988 * pow(map(ir_val[NB_SAMPLE / 2], 0, 1023, 0, 5000)/1000.0, -1.173);
-        #elif defined(SPARK)
-          distanceCM = 29.988 * pow(map(ir_val[NB_SAMPLE / 2], 0, 4095, 0, 5000)/1000.0, -1.173);
-        #endif
-
-    } 
-    else if (_model==1084) 
-    {
-        
-        // Different expressions required as the Photon has 12 bit ADCs vs 10 bit for Arduinos
-        #ifdef ARDUINO
-          distanceCM = 29.988 * pow(map(ir_val[NB_SAMPLE / 2], 0, 1023, 0, 5000)/1000.0, -1.173);
-        #elif defined(SPARK)
-          distanceCM = 29.988 * pow(map(ir_val[NB_SAMPLE / 2], 0, 4095, 0, 5000)/1000.0, -1.173);
-        #endif
-
-    } 
-    else if (_model==20150)
-    {
-
-        // Previous formula used by  Dr. Marcal Casas-Cartagena
-        // puntualDistance=61.573*pow(voltFromRaw/1000, -1.1068);
-        
-        // Different expressions required as the Photon has 12 bit ADCs vs 10 bit for Arduinos
-        #ifdef ARDUINO
-          distanceCM = 60.374 * pow(map(ir_val[NB_SAMPLE / 2], 0, 1023, 0, 5000)/1000.0, -1.16);
-        #elif defined(SPARK)
-          distanceCM = 60.374 * pow(map(ir_val[NB_SAMPLE / 2], 0, 4095, 0, 5000)/1000.0, -1.16);
-        #endif
-
-    }
-
-    return distanceCM;
-}
-*/
-
-/*
-// linear interpolation method
-int SharpIR::distance()
-{
-
-	int ir_val[NB_SAMPLE];
-
-	for (int i=0; i<NB_SAMPLE; i++){
-        // Read analog value
-        ir_val[i] = analogRead(_irPin);
-    }
-    
-    // Sort it 
-    sort(ir_val,NB_SAMPLE);
-
-    int mediumIR_val = ir_val[NB_SAMPLE/2];
-
-	for(int i = 0; i < _numOfDataPoints - 1; i++)
-	{
-		if(_analogCalData[i] < mediumIR_val && mediumIR_val < _analogCalData[i+1])
-		{
-			// https://blogs.ntu.edu.sg/scemdp-201718s1-g14/ir-sensors/
-			// linear interpolation: distance = y1 + (x - x1) * ((y2 - y1) / (x2 - x1))
-			return _calDistance[i+1] + (mediumIR_val - _analogCalData[i+1]) * ((_calDistance[i] -  _calDistance[i+1]) / (_analogCalData[i] - _analogCalData[i+1]));
-		}
-
-	}
-}
-*/
-
 
 // method to be used
 float SharpIR::distance()
@@ -429,30 +268,6 @@ float SharpIR::distance()
 			// for 70cm
 			else if (calibratedDist < 88.0)
 				return calibratedDist - 15.0;
-
-
-			// offset:
-			// for 20cm
-			// if (calibratedDist < 26.0)
-			// 	return calibratedDist - 3.0;
-			// // for 30cm
-			// else if (calibratedDist < 35.0)
-			// 	return calibratedDist;
-			// // for 40cm
-			// else if (calibratedDist < 45.0)
-			// 	return calibratedDist - 1.0;
-			// // for 50cm
-			// else if (calibratedDist < 60.0)
-			// 	return calibratedDist - 3.0;
-			// // for 60cm
-			// else if (calibratedDist < 70.0)
-			// 	return calibratedDist - 5.0;
-			// // for 70cm
-			// else if (calibratedDist < 85.0)
-			// 	return calibratedDist - 7.0;
-			// // for 80cm
-			// else if (calibratedDist < 97.0)
-			// 	return calibratedDist - 13.0;
 
 			return calibratedDist;
 	}
